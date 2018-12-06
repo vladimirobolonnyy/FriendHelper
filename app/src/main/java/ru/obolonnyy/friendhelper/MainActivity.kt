779e0e88573076f7547dc1b7e9a1ad89acddd303
Obolonnyy Vladimir <obolonnyyv@gmail.com>
2018-12-06 17:02:33 +0300

    Initial commit

---
+++ app/src/main/java/ru/obolonnyy/friendhelper/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.obolonnyy.friendhelper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.obolonnyy.friendhelper.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
